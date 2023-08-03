f9c6a4f94c9c86c5fb14f21a5546c4ce83a17b7f
Haydz6 <cookiegamespa@gmail.com>
2023-08-03 21:25:15 +1000

    fixed qol button disappearing

--- js/pages/settings/settingsdropdown.js
+++ js/pages/settings/settingsdropdown.js
@@ -1,15 +1,17 @@
 async function AddOptionToSettingsDropdown(){
-    const Dropdown = await WaitForId("settings-popover-menu")
+    ChildAdded(await WaitForId("navbar-settings"), true, async function(){
+        const Dropdown = await WaitForId("settings-popover-menu")
 
-    const List = document.createElement("li")
-    const Button = document.createElement("a")
+        const List = document.createElement("li")
+        const Button = document.createElement("a")
 
-    Button.className = "rbx-menu-item"
-    Button.href = "https://www.roblox.com/my/account?tab=robloxqol"
-    Button.innerText = "QoL Settings"
+        Button.className = "rbx-menu-item"
+        Button.href = "https://www.roblox.com/my/account?tab=robloxqol"
+        Button.innerText = "QoL Settings"
 
-    List.appendChild(Button)
-    Dropdown.insertBefore(List, Dropdown.firstChild)
+        List.appendChild(Button)
+        Dropdown.insertBefore(List, Dropdown.firstChild)
+    })
 }
 
 AddOptionToSettingsDropdown()
