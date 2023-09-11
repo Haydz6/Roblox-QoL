f4adc57e295b54450681ac6b7ba32fb5c37cfc91
Haydz6 <cookiegamespa@gmail.com>
2023-09-12 00:32:24 +1000

    Look for sortid, not name for locale support

--- js/pages/fixfavouritespage/replacehomelink.js
+++ js/pages/fixfavouritespage/replacehomelink.js
@@ -1,17 +1,22 @@
 IsFeatureEnabled("FixFavouritesPage").then(async function(Enabled){
     if (!Enabled) return
 
-    while (true){
-        const SeeAllButtons = document.getElementsByClassName("btn-secondary-xs see-all-link-icon btn-more")
+    // while (true){
+    //     const SeeAllButtons = document.getElementsByClassName("btn-secondary-xs see-all-link-icon btn-more")
 
-        for (let i = 0; i < SeeAllButtons.length; i++){
-            const Button = SeeAllButtons[i]
-            if (Button.href.search("sortName/v2/Favorites") > -1){
-                Button.href = "https://roblox.com/discover#/sortName?sort=Favorites"
-                return
-            }
-        }
+    //     for (let i = 0; i < SeeAllButtons.length; i++){
+    //         const Button = SeeAllButtons[i]
+    //         if (Button.href.search("sortName/v2/Favorites") > -1){
+    //             Button.href = "https://roblox.com/discover#/sortName?sort=Favorites"
+    //             return
+    //         }
+    //     }
 
-        await sleep(20)
-    }
+    //     await sleep(20)
+    // }
+
+    const Container = await WaitForClass("game-home-page-container")
+    const [Title] = await SearchForRow(Container, 100000001)
+    const SeeAll = await WaitForClassPath(Title, "see-all-link-icon")
+    SeeAll.href = "https://roblox.com/discover#/sortName?sort=Favorites"
 })
