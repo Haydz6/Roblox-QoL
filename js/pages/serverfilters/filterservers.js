83daed46fd1fe033f6bb9800f98fe3252d7a2a18
Haydz6 <cookiegamespa@gmail.com>
2023-09-13 11:57:14 +1000

    Fix excludeFullServers

--- js/pages/serverfilters/filterservers.js
+++ js/pages/serverfilters/filterservers.js
@@ -1,3 +1,3 @@
 async function GetRobloxServers(Cursor, Ascending, ExcludeFullGames, MaxServers){
-    return await RequestFunc(`https://games.roblox.com/v1/games/${await GetPlaceIdFromGamePage()}/servers/0?sortOrder=${Ascending && 1 || 2}&excludeFullGames=${ExcludeFullGames || false}&limit=${MaxServers || 100}&cursor=${Cursor || ""}`, "GET", undefined, undefined, true)
+    return await RequestFunc(`https://games.roblox.com/v1/games/${await GetPlaceIdFromGamePage()}/servers/0?sortOrder=${Ascending && 1 || 2}&excludeFullGames=${ExcludeFullGames && true || false}&limit=${MaxServers || 100}&cursor=${Cursor || ""}`, "GET", undefined, undefined, true)
 }
