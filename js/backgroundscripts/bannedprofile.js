1ce31b064b829251e7376b0fb48aeeef739d8af5
Haydz6 <cookiegamespa@gmail.com>
2023-09-20 21:42:33 +1000

    Fixed banned-user not always redirecting

--- js/backgroundscripts/bannedprofile.js
+++ js/backgroundscripts/bannedprofile.js
@@ -1,8 +1,6 @@
 chrome.webRequest.onBeforeRedirect.addListener(function(Document){
-    console.log(Document.redirectUrl.includes("roblox.com/request-error?code=404"))
-    console.log(Document.url.includes("roblox.com/users/"))
     if (Document.redirectUrl.includes("roblox.com/request-error?code=404") && Document.url.includes("roblox.com/users/")){
         const UserId = Document.url.split("users/")[1].split("/")[0]
         chrome.tabs.update(Document.tabId, {url: "https://roblox.com/banned-user/"+UserId})
     }
-}, {urls: ["*://*.roblox.com/users/*/profile"]})
+}, {urls: ["*://*.roblox.com/users/*/profile*"]})
