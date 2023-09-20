cf7aca1059c6b140131924cbc5c5dd1b6f81522f
Haydz6 <cookiegamespa@gmail.com>
2023-09-20 11:19:37 +1000

    Fix infinite load

--- js/pages/generalfixes/minimizeprivateservers.js
+++ js/pages/generalfixes/minimizeprivateservers.js
@@ -1,26 +1,28 @@
-IsFeatureEnabled("MinimizePrivateServers").then(async function(Enabled){
+IsFeatureEnabled("MinimizePrivateServers").then(function(Enabled){
     if (!Enabled) return
 
-    const PrivateServers = await WaitForId("rbx-private-servers")
-    const Header = await WaitForClassPath(PrivateServers, "container-header")
-    const Banner = await WaitForClassPath(PrivateServers, "create-server-banner")
-    const Section = await WaitForClassPath(PrivateServers, "section")
+    setTimeout(async function(){
+        const PrivateServers = await WaitForId("rbx-private-servers")
+        const Header = await WaitForClassPath(PrivateServers, "container-header")
+        const Banner = await WaitForClassPath(PrivateServers, "create-server-banner")
+        const Section = await WaitForClassPath(PrivateServers, "section")
 
-    Header.style = "cursor: pointer;"
+        Header.style = "cursor: pointer;"
 
-    const Minimize = document.createElement("span")
-    Minimize.className = "icon-up-16x16"
-    Minimize.style = "margin-left: 3px;"
+        const Minimize = document.createElement("span")
+        Minimize.className = "icon-up-16x16"
+        Minimize.style = "margin-left: 3px;"
 
-    let Opened = true
-    Header.addEventListener("click", function(){
-        Opened = !Opened
-        Minimize.className = `icon-${Opened ? "up" : "down"}-16x16`
-        Banner.style.display = Opened ? "" : "none"
-        Section.style.display = Opened ? "" : "none"
-    })
+        let Opened = true
+        Header.addEventListener("click", function(){
+            Opened = !Opened
+            Minimize.className = `icon-${Opened ? "up" : "down"}-16x16`
+            Banner.style.display = Opened ? "" : "none"
+            Section.style.display = Opened ? "" : "none"
+        })
 
-    const Tooltip = Header.getElementsByClassName("tooltip-container")[0]
-    if (Tooltip) Header.insertBefore(Minimize, Tooltip)
-    else Header.appendChild(Minimize)
+        const Tooltip = Header.getElementsByClassName("tooltip-container")[0]
+        if (Tooltip) Header.insertBefore(Minimize, Tooltip)
+        else Header.appendChild(Minimize)
+    }, 0)
 })
