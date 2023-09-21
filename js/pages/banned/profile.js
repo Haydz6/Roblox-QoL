3275e4a212cd1c352d1b8ff20aa4016a76cd2c53
Haydz6 <cookiegamespa@gmail.com>
2023-09-21 18:24:17 +1000

    Group support

--- js/pages/banned/profile.js
+++ js/pages/banned/profile.js
@@ -101,6 +101,7 @@ IsFeatureEnabled("ViewBannedUser").then(async function(Enabled){
             })
 
             const BadgeList = document.getElementById("player-badges-container").getElementsByClassName("badge-list")[0]
+            document.getElementById("player-badges-container").getElementsByClassName("btn-more")[0].remove()
             const BadgeClone = BadgeList.getElementsByClassName("list-item")[0].cloneNode(true)
             BadgeList.replaceChildren()
 
@@ -148,6 +149,7 @@ IsFeatureEnabled("ViewBannedUser").then(async function(Enabled){
             })
 
             const FavouritesList = document.getElementsByClassName("favorite-games-container")[0].getElementsByClassName("game-cards")[0]
+            document.getElementsByClassName("favorite-games-container")[0].getElementsByClassName("btn-more")[0].remove()
             const GameCardClone = FavouritesList.getElementsByClassName("list-item")[0].cloneNode(true)
             FavouritesList.replaceChildren()
 
@@ -202,7 +204,7 @@ IsFeatureEnabled("ViewBannedUser").then(async function(Enabled){
                     BadgeElement.getElementsByClassName("game-card-thumb")[0].src = Badge.Thumbnail.Url
                     BadgeElement.getElementsByClassName("game-card-name")[0].innerText = UniverseLookup[Badge.Item.UniverseId]?.name || "???"
                     BadgeElement.getElementsByClassName("game-card-name")[0].title = UniverseLookup[Badge.Item.UniverseId]?.name || "???"
-                    BadgeElement.getElementsByClassName("playing-counts-label")[0].innerText = typeof(Playing) === "number" ? AbbreviateNumber(Playing) : Playing
+                    BadgeElement.getElementsByClassName("playing-counts-label")[0].innerText = typeof(Playing) === "number" ? AbbreviateNumber(Playing, 0, true) : Playing
                     BadgeElement.getElementsByClassName("playing-counts-label")[0].title = Playing
 
                     let LikeRatio
@@ -657,6 +659,139 @@ IsFeatureEnabled("ViewBannedUser").then(async function(Enabled){
                 CalcuatePlaceVisits(PlaceVisits, Cursor)
             }
             CalcuatePlaceVisits()
+
+            const GroupsContainer = document.querySelector("[groups-showcase]")
+            const GroupHeader = GroupsContainer.getElementsByClassName("container-header")[0]
+            const Switchers = GroupsContainer.getElementsByClassName("profile-slide-container")[0]
+            const GroupSliderContainer = Switchers.getElementsByClassName("switcher")[0]
+            const GroupGridContainer = Switchers.getElementsByTagName("groups-showcase-grid")[0]
+
+            const GroupSlider = GroupSliderContainer.getElementsByClassName("hlist")[0]
+            const GroupGrid = GroupGridContainer.getElementsByClassName("hlist")[0]
+
+            const GroupGridClone = GroupGrid.children[0].cloneNode(true)
+            const GroupSlideClone = GroupSlider.children[0].cloneNode(true)
+            GroupSlideClone.className = GroupSlideClone.className.replace(" active", "")
+
+            let CurrentGroupSelected = 0
+            let PriorGroupSlide
+            const GroupSlides = []
+
+            GroupSlider.replaceChildren()
+            GroupGrid.replaceChildren()
+
+            function LoadGroupSlideElement(){
+                if (PriorGroupSlide) PriorGroupSlide.className = PriorGroupSlide.className.replace(" active", "")
+                const NewSlide = GroupSlides[CurrentGroupSelected]
+                if (NewSlide){
+                    PriorGroupSlide = NewSlide
+                    NewSlide.className += " active"
+                }
+            }
+
+            RequestFunc(`https://groups.roblox.com/v1/users/${UserId}/groups/roles`, "GET").then(async function([Success, Body]){
+                if (!Success) return GroupsContainer.remove()
+
+                const GroupIcons = {}
+
+                const Groups = Body.data
+                if (Groups.length === 0){
+                    return GroupsContainer.remove()
+                }
+
+                const GroupIds = []
+                for (let i = 0; i < Groups.length; i++){
+                    GroupIds.push(Groups[i].group.id)
+                }
+
+                const [ThumbSuccess, Thumbnails] = await RequestFunc(`https://thumbnails.roblox.com/v1/groups/icons?groupIds=${GroupIds.join(",")}&size=150x150&format=Png&isCircular=false`, "GET")
+                if (ThumbSuccess){
+                    const Data = Thumbnails.data
+                    for (let i = 0; i < Data.length; i++){
+                        GroupIcons[Data[i].targetId] = Data[i].imageUrl
+                    }
+                }
+
+                for (let i = 0; i < Groups.length; i++){
+                    const Group = Groups[i]
+                    const Icon = GroupIcons[Group.group.id] || "https://tr.rbxcdn.com/53eb9b17fe1432a809c73a13889b5006/420/420/Image/Png"
+                    const URL = `https://roblox.com/groups/${Group.group.id}`
+
+                    const Grid = GroupGridClone.cloneNode(true)
+                    Grid.getElementsByClassName("game-card-container")[0].href = URL
+                    Grid.getElementsByClassName("thumbnail-2d-container")[0].getElementsByTagName("img")[0].src = Icon
+
+                    Grid.getElementsByClassName("game-card-name")[0].innerText = Group.group.name
+                    if (!Group.group.hasVerifiedBadge) Grid.getElementsByClassName("verified-badge-icon-group-showcase-grid-rendered")[0].parentElement.remove()
+                    Grid.getElementsByClassName("game-card-name-secondary")[0].innerText = `${numberWithCommas(Group.group.memberCount)} Members`
+                    Grid.getElementsByClassName("game-card-name-secondary")[1].innerText = Group.role.name
+
+                    GroupGrid.appendChild(Grid)
+
+                    const Slider = GroupSlideClone.cloneNode(true)
+                    Slider.getElementsByClassName("slide-item-emblem-container")[0].getElementsByTagName("a")[0].href = URL
+                    Slider.getElementsByClassName("thumbnail-2d-container")[0].getElementsByTagName("img")[0].src = Icon
+
+                    if (!Group.group.hasVerifiedBadge) Slider.getElementsByClassName("verified-badge-icon-group-carousel-rendered")[0].parentElement.remove()
+
+                    Slider.getElementsByClassName("group-title-with-badges")[0].href = URL
+                    if (!Group.group.hasVerifiedBadge) Slider.getElementsByClassName("group-title-with-badges")[0].className = Slider.getElementsByClassName("group-title-with-badges")[0].className.replace("group-title-with-badge", "")
+                    Slider.getElementsByClassName("slide-item-name")[0].innerText = Group.group.name
+                    if (!Group.group.hasVerifiedBadge) Slider.getElementsByClassName("slide-item-name")[0].className = Slider.getElementsByClassName("slide-item-name")[0].className.replace("truncate-with-verified-badge", "")
+
+                    Slider.getElementsByClassName("slide-item-description")[0].innerText = Group.group.description
+
+                    const Statistics = Slider.getElementsByClassName("slide-item-stats")[0].getElementsByClassName("hlist")[0]
+                    Statistics.children[0].children[1].innerText = AbbreviateNumber(Group.group.memberCount, 0)
+                    Statistics.children[1].children[1].innerText = Group.role.name
+
+                    GroupSlider.appendChild(Slider)
+                    GroupSlides.push(Slider)
+                }
+
+                LoadGroupSlideElement()
+            })
+
+            const GroupButtonTypes = GroupHeader.getElementsByClassName("container-buttons")[0]
+            const GroupSlideButton = GroupButtonTypes.getElementsByClassName("btn-generic-slideshow-xs")[0]
+            const GroupGridButton = GroupButtonTypes.getElementsByClassName("btn-generic-grid-xs")[0]
+
+            GroupSlideButton.addEventListener("click", function(){
+                GroupSlideButton.className = GroupSlideButton.className.replace("btn-secondary-xs", "btn-control-xs")
+                GameGridButton.className = GroupSlideButton.className.replace("btn-control-xs", "btn-secondary-xs")
+
+                GroupSlider.style.display = ""
+                GroupGrid.style.display = "none"
+            })
+
+            GroupGridButton.addEventListener("click", function(){
+                GroupGridButton.className = GroupGridButton.className.replace("btn-secondary-xs", "btn-control-xs")
+                GameSlideButton.className = GroupGridButton.className.replace("btn-control-xs", "btn-secondary-xs")
+
+                GroupGrid.style.display = ""
+                GroupSlider.style.display = "none"
+            })
+
+            GroupSlider.style.display = ""
+            GroupGrid.style.display = "none"
+
+            GroupSliderContainer.getElementsByClassName("carousel-control left")[0].addEventListener("click", function(){
+                if (LoadingNextGames) return
+
+                CurrentGroupSelected--
+                if (CurrentGroupSelected < 0) CurrentGroupSelected = Games.length - 1
+                LoadGroupSlideElement()
+            })
+
+            GroupSliderContainer.getElementsByClassName("carousel-control right")[0].addEventListener("click", function(){
+                if (LoadingNextGames) return
+
+                CurrentGroupSelected++
+                if (CurrentGroupSelected > Games.length) CurrentGroupSelected = 0
+                LoadGroupSlideElement()
+            })
+
+            GroupGridContainer.getElementsByClassName("load-more-button")[0].remove()
 
             document.getElementsByClassName("abuse-report-link")[0].remove()
             Content.getElementsByClassName("enable-three-dee")[0].remove()
