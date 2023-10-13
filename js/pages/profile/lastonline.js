80ebdddf9f4077bd8e40949593202e7411f1575f
Haydz6 <cookiegamespa@gmail.com>
2023-10-13 19:38:59 +1100

    Retry for last online

--- js/pages/profile/lastonline.js
+++ js/pages/profile/lastonline.js
@@ -8,7 +8,23 @@ IsFeatureEnabled("LastOnline").then(async function(Enabled){
     ProfileStat.innerHTML = `<p class="text-label">Last Online</p><p title class="text-lead">...</p>`
     const LastOnlineLabel = ProfileStat.getElementsByClassName("text-lead")[0] //make all profile-stat 33.33%
 
-    RequestFunc("https://presence.roblox.com/v1/presence/users", "POST", {"Content-Type": "application/json"}, JSON.stringify({userIds: [TargetId]}), true).then(function([Success, Body]){
+    async function SetLastOnline(){
+        const [Success, Body] = await RequestFunc("https://presence.roblox.com/v1/presence/last-online", "POST", {"Content-Type": "application/json"}, JSON.stringify({userIds: [TargetId]}), true)
+
+        if (!Success){
+            LastOnlineLabel.innerText = "Failed"
+            return false
+        }
+        
+        const LastOnline = new Date(Body.lastOnlineTimestamps[0].lastOnline)
+
+        LastOnlineLabel.title = LastOnline.toLocaleDateString(undefined, {hour: "numeric", minute: "numeric", second: "numeric", hour12: true})
+        LastOnlineLabel.innerText = SecondsToLengthSingle(Date.now()/1000 - LastOnline.getTime()/1000, true) + " ago"
+
+        return true
+    }
+
+    RequestFunc("https://presence.roblox.com/v1/presence/users", "POST", {"Content-Type": "application/json"}, JSON.stringify({userIds: [TargetId]}), true).then(async function([Success, Body]){
         if (!Success){
             LastOnlineLabel.innerText = "Failed"
             return
@@ -18,16 +34,10 @@ IsFeatureEnabled("LastOnline").then(async function(Enabled){
             return
         }
 
-        RequestFunc("https://presence.roblox.com/v1/presence/last-online", "POST", {"Content-Type": "application/json"}, JSON.stringify({userIds: [TargetId]}), true).then(function([Success, Body]){
-            if (!Success){
-                LastOnlineLabel.innerText = "Failed"
-                return
-            }
-            const LastOnline = new Date(Body.lastOnlineTimestamps[0].lastOnline)
-
-            LastOnlineLabel.title = LastOnline.toLocaleDateString(undefined, {hour: "numeric", minute: "numeric", second: "numeric", hour12: true})
-            LastOnlineLabel.innerText = SecondsToLengthSingle(Date.now()/1000 - LastOnline.getTime()/1000, true) + " ago"
-        })
+        for (let i = 0; i < 3; i++){
+            if (await SetLastOnline()) break
+            await sleep(1500)
+        }
     })
 
     const Container = await WaitForClass("profile-stats-container")
