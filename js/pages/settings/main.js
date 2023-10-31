caf6835a67064f74ea71bfa124ae3315c0758279
Haydz6 <cookiegamespa@gmail.com>
2023-10-31 21:09:28 +1100

    Roblox patched hide offline

--- js/pages/settings/main.js
+++ js/pages/settings/main.js
@@ -56,13 +56,13 @@ const Settings = {
             Title: "Show last online on Profile",
             Description: "Shows last online of user on their profile"
         },
-        HideOffline: {
-            Title: "Appear offline (Website Only)",
-            Description: "Makes you appear offline to other users while browsing the website. (This only works on the website, it is a limitation of roblox)"
-            // Supported: async function(){
-            //     return await chrome.runtime.sendMessage({type: "FeatureSupported", name: "hideoffine"})
-            // }
-        },
+        // HideOffline: {
+        //     Title: "Appear offline (Website Only)",
+        //     Description: "Makes you appear offline to other users while browsing the website. (This only works on the website, it is a limitation of roblox)"
+        //     // Supported: async function(){
+        //     //     return await chrome.runtime.sendMessage({type: "FeatureSupported", name: "hideoffine"})
+        //     // }
+        // },
         ShowFollowsYou: {
             Title: "Follows You Indicator",
             Description: "Shows if a user is following you."
