b3235fb7cd02abe7a9c3c4935d931925f59d5c63
Haydz6 <cookiegamespa@gmail.com>
2023-11-24 21:17:11 +1100

    FIx add best friend missing from react header

--- js/pages/bestfriend/profile.js
+++ js/pages/bestfriend/profile.js
@@ -12,43 +12,52 @@ IsFeatureEnabled("BestFriends").then(async function(Enabled){
     }
 
     let AreFriends = false
+    let List
+
+    function AddButtonToList(){
+        if (document.getElementById("add-best-friend")) return
+
+        const Item = document.createElement("li")
+        const Button = document.createElement("button")
+        Button.role = "button"
+        Button.id = "add-best-friend"
+        LastButton = Button
+        UpdateStatus()
+
+        Button.addEventListener("click", async function(){
+            IsPinned = !IsPinned
+            UpdateStatus()
+
+            const OriginalPinned = IsPinned
+            const [Success] = await RequestFunc(WebServerEndpoints.BestFriends+"pin", "POST", {"Content-Type": "application/json"}, JSON.stringify({Pinned: IsPinned, UserId: TargetId}))
+            if (!Success){
+                IsPinned = OriginalPinned
+                UpdateStatus()
+            }
+        })
+
+        Item.appendChild(Button)
+        List.insertBefore(Item, List.children[0])
+    }
 
     function UpdateAreFriends(Event){
         AreFriends = Event.detail
+        if (AreFriends && List) AddButtonToList()
     }
     InjectScript("AreFriendedProfile")
     document.addEventListener("RobloxQoL.areFriended", UpdateAreFriends)
 
-    const MoreHeader = await WaitForId("profile-header-more")
-    ChildAdded(MoreHeader, true, async function(Popover){
-        const PopoverContent = Popover.getElementsByClassName("popover-content")[0]
-        if (!PopoverContent) return
+    const MoreHeader = await WaitForClassPath(await WaitForQuerySelector(".profile-header:not(.hidden)"), "profile-header-more")
+    const DropdownContainer = await WaitForClassPath(MoreHeader, "profile-dropdown")
 
-        ChildAdded(PopoverContent, true, function(List){
-            if (!List.className.includes("dropdown-menu")) return
-            if (!AreFriends) return
+    console.log(DropdownContainer)
+    ChildAdded(DropdownContainer, true, async function(NewList){
+        if (!NewList.className.includes("dropdown-menu")) return
+        List = NewList
+        List.style["max-height"] = "300px"
+        if (!AreFriends) return
 
-            const Item = document.createElement("li")
-            const Button = document.createElement("button")
-            Button.role = "button"
-            LastButton = Button
-            UpdateStatus()
-
-            Button.addEventListener("click", async function(){
-                IsPinned = !IsPinned
-                UpdateStatus()
-
-                const OriginalPinned = IsPinned
-                const [Success] = await RequestFunc(WebServerEndpoints.BestFriends+"pin", "POST", {"Content-Type": "application/json"}, JSON.stringify({Pinned: IsPinned, UserId: TargetId}))
-                if (!Success){
-                    IsPinned = OriginalPinned
-                    UpdateStatus()
-                }
-            })
-
-            Item.appendChild(Button)
-            List.insertBefore(Item, List.children[0])
-        })
+        AddButtonToList()
     })
 
     const [Success, Body] = await RequestFunc(WebServerEndpoints.BestFriends+"ispinned?userId="+TargetId, "GET")
