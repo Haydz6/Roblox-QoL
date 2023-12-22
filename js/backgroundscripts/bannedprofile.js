8a1522616d4c6bafd1004e33b663324fadcdb102
Haydz6 <cookiegamespa@gmail.com>
2023-12-22 22:07:07 +1100

    Safari support for banned profile

--- js/backgroundscripts/bannedprofile.js
+++ js/backgroundscripts/bannedprofile.js
@@ -9,5 +9,28 @@ try {
         }
     }, {urls: ["*://*.roblox.com/users/*/profile*", "*://*.roblox.com/users/*/friends*"]})
 } catch {
-    BannedUsersSupported = false
+    const NavigationTabs = {}
+
+    chrome.webNavigation.onBeforeNavigate.addListener(function(Navigation){
+        if (Navigation.frameType === "outermost_frame" && Navigation.url.includes("roblox.com/users/")){
+            const Id = Navigation.tabId
+
+            NavigationTabs[Id] = Navigation
+            setTimeout(function(){
+                delete NavigationTabs[Id]
+            }, 1000*60)
+        }
+    })
+    chrome.webNavigation.onCommitted.addListener(function(Navigation){
+        if (Navigation.frameType === "outermost_frame" && Navigation.url.includes("roblox.com/request-error?code=404")){
+            const PriorNavigation = NavigationTabs[Navigation.tabId]
+            if (!PriorNavigation) return
+
+            const Split = PriorNavigation.url.split("users/")[1].split("/")
+            const UserId = Split[0]
+            chrome.tabs.update(PriorNavigation.tabId, {url: "https://www.roblox.com/banned-user/"+UserId+"/"+Split[1]})
+
+            delete NavigationTabs[Navigation.tabId]
+        }
+    })
 } //Stop errors from safari
