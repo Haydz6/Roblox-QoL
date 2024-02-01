e64d711385919467dffbc1159d217b6adcfa0981
Haydz6 <cookiegamespa@gmail.com>
2024-02-01 15:31:35 +1100

    Fix friends activity not appearing because of home page edits

--- js/pages/friendsactivity/home.js
+++ js/pages/friendsactivity/home.js
@@ -1,29 +1,31 @@
-IsFeatureEnabled("FriendsActivity").then(async function(Enabled){
+IsFeatureEnabled("FriendsActivity").then(function(Enabled){
     if (!Enabled) return
 
-    let GamesList = await WaitForClass("game-home-page-container")
-    if (await IsFeatureEnabled("TemporaryHomePageContainerFix")) GamesList = (await WaitForClassPath(GamesList, "game-carousel")).parentNode
-    
-    const [Success, Games] = await RequestFunc(WebServerEndpoints.Friends+"sort?type=Some", "GET")
-    if (!Success){
-        GameCarousel.innerText = "Failed to fetch"
-        Spinner.remove()
-        return
-    }
-    if (Games.length === 0) return
+    setTimeout(async function(){
+        let GamesList = await WaitForClass("game-home-page-container")
+        if (await IsFeatureEnabled("TemporaryHomePageContainerFix")) GamesList = (await WaitForClassPath(GamesList, "game-carousel")).parentNode
+        
+        const [Success, Games] = await RequestFunc(WebServerEndpoints.Friends+"sort?type=Some", "GET")
+        if (!Success){
+            GameCarousel.innerText = "Failed to fetch"
+            Spinner.remove()
+            return
+        }
+        if (Games.length === 0) return
 
-    const [ContainerHeader, _, HeaderButton] = CreateContainerHeader("Friends Activity", `https://www.roblox.com/discover#/sortName?sort=FriendsActivity`)
-    HeaderButton.innerText = "Friends Activity"
+        const [ContainerHeader, _, HeaderButton] = CreateContainerHeader("Friends Activity", `https://www.roblox.com/discover#/sortName?sort=FriendsActivity`)
+        HeaderButton.innerText = "Friends Activity"
 
-    const GameCarousel = CreateGameCarousel()
+        const GameCarousel = CreateGameCarousel()
 
-    const Spinner = CreateSpinner()
-    GameCarousel.appendChild(Spinner)
+        const Spinner = CreateSpinner()
+        GameCarousel.appendChild(Spinner)
 
-    const [RecommendedTitle] = await SearchForRow(GamesList, 100000000)
+        const [RecommendedTitle] = await SearchForRowWithGames(GamesList, 100000000)
 
-    GamesList.insertBefore(GameCarousel, RecommendedTitle)
-    GamesList.insertBefore(ContainerHeader, GameCarousel)
+        GamesList.insertBefore(GameCarousel, RecommendedTitle)
+        GamesList.insertBefore(ContainerHeader, GameCarousel)
 
-    CreateGameCardsFromUniverseIds(Games, GameCarousel, null, null, null, Spinner)
+        CreateGameCardsFromUniverseIds(Games, GameCarousel, null, null, null, Spinner)
+    }, 0)
 })
